cc214b2531270429dadf98a204a1cfb0738b472a
Wilco <wilco@matthijssen.xyz>
2021-09-19 15:27:43 +0200

    Changed structure so tetromino is part of playfield

--- lib/tetris/tetris.cpp
+++ lib/tetris/tetris.cpp
@@ -3,183 +3,123 @@
 #include <random>
 #include <algorithm>
 
-#include <tetris.h>
+#include "tetris.h"
 
+/* --------------------------------------------------
+                    PUBLIC
+---------------------------------------------------*/
 
 void Tetris::begin(){
-    // clear playfield
-    for(auto & row : playfield){
-        for(auto & spot : row){
-            spot = 0;
-        }
-    }
-    
-    gameOver=false;
+    for(auto& row: playfield){  row.fill(0);  }
     score=0;
-    spawn_new_tetromino();
+    spawn_tetromino_if_fits();
 }
 
-
-const bool Tetris::move_left(){
-    --tetromino_x; 
-
-    const bool is_possible = does_tetromino_fit();
-    if(!is_possible){ ++tetromino_x; }
-
-    return is_possible;
-}
+void Tetris::rotate_left(){    move_tetromino_if_fits(Point{ 1, 0},  1); }
+void Tetris::rotate_right(){   move_tetromino_if_fits(Point{ 0, 0}, -1); }
 
 
-const bool Tetris::move_right(){
-    ++tetromino_x; 
+void Tetris::move_left(){      move_tetromino_if_fits(Point{ 1, 0}, 0); }
+void Tetris::move_right(){     move_tetromino_if_fits(Point{-1, 0}, 0); }
 
-    const bool is_possible = does_tetromino_fit();
-    if(!is_possible){  --tetromino_x;  }
-
-    return is_possible;
-}
-
-
-const bool Tetris::move_down(){
-    --tetromino_y;
-    if(does_tetromino_fit()){ 
-        ++tetromino_y; 
-        return false;
+void Tetris::move_down(){
+    if(!move_tetromino_if_fits( Point{0, -1}, 0)){
+        check_rows();
+        spawn_tetromino_if_fits();
     }
-    
-    place_tetromino_on(playfield);
-    check_rows();
-    spawn_new_tetromino();
-    
-    return true;
 }
 
+/* --------------------------------------------------
+                    PRIVATE
+---------------------------------------------------*/
 
-
-const bool Tetris::rotate(){ 
-    rotation = (rotation + 1) % 4;
-
-    const bool is_possible = does_tetromino_fit();
-    if(!is_possible){ rotation = (rotation - 1) % 4; }
-
-    return is_possible;
-}
-
-
-
-
-const std::array<std::array<byte, 10>, 20> Tetris::get_playfield() const{
-    auto tempPlayfield = playfield;
-    place_tetromino_on(tempPlayfield);
-
-    return tempPlayfield;
-}
-
-
-void Tetris::remove_row(const int8_t & index){
-    for(int8_t i = index; i< HEIGHT-1; ++i){
-        playfield[i]= playfield[i+1];
+void Tetris::set_tetromino(const int8_t& value){
+    for(const auto& point: tetromino_positions){ 
+        playfield[point.y][point.x] = value; 
     }
-    
-    // //Clear top row
-    auto& top_row = playfield[HEIGHT-1];
-    std::fill(top_row.begin(), top_row.end(), 0);
- 
 }
 
 
-const bool Tetris::is_row_full(const int8_t & index) const{
-    const auto& row = playfield[index];
-    auto is_spot_filled = [](const int8_t& spot){ return spot != 0; };
-    auto is_row_full = [](const std::array<WIDTH, uint8_t> & row){ return std::all_of(row.begin(), row.end(), is_spot_filled(row)); };
-    return is_row_full(row);
+bool Tetris::move_tetromino_if_fits(const Point& delta, const int8_t& rotation){
+    // Remove tetromino from playfield to not interfere with does_tetromino_fit and save color to later revert this action with the same color
+    const auto color = playfield[tetromino_positions.front().y][tetromino_positions.front().x];
+    set_tetromino(0);
+
+    const array<Point, 4> positions = get_new_tetromino_positions(delta, rotation);
+    const bool can_position_change = does_tetromino_fit(positions);
+
+    if(can_position_change){
+        tetromino_origin    += delta;
+        tetromino_positions  = positions;
+    }
+    set_tetromino(color);
+    return can_position_change;
+}
+
+
+array<Point, 4> Tetris::get_new_tetromino_positions(const Point& delta, const int8_t& rotation) const{
+    array<Point, 4> new_positions = tetromino_positions;
+    const Point origin = tetromino_origin + delta;
+    for(auto& point: new_positions){ 
+        point = get_rotated_position(origin, point + delta, rotation);
+    }
+    return new_positions;
+}
+
+
+bool Tetris::does_tetromino_fit(const array<Point, 4>& positions) const{
+    const auto is_space_free = [&](const Point& pos ){
+                                            return  pos.x >= 0    && 
+                                                    pos.x < width && 
+                                                    pos.y >= 0    && 
+                                                    playfield[pos.y][pos.x] == 0; };
+                                                    
+    return std::all_of(positions.begin(), positions.end(), is_space_free);
 }
 
 
 void Tetris::check_rows(){
+    uint8_t full_rows = 0;
 
-    increase_score(full_rows)
-    remove_row(full_rows)
-
-    int8_t full_rows = 0;
-    for(int8_t i = HEIGHT - 1; i >= 0; --i){
-        if( is_row_full(i) ){ 
-            remove_row(i); 
-            ++full_rows; 
-        }
-    }
-}
-
-void Tetris::place_tetromino_on(std::array<std::array<byte, WIDTH>, HEIGHT>& given_playfield) const{
-    for(int8_t i = tetromino_x; i <  tetromino_x + 4; ++i){
-        for(int8_t j =  tetromino_y; j <  tetromino_y + 4; ++j){
-            given_playfield[i][j] = TetrominoShapes[currentTetromino][i + ((tetromino_y - j) * 4)];
+    for(auto row_it = playfield.begin(); row_it != playfield.end(); ++row_it){
+        if(std::count(row_it->begin(), row_it->end(), 0) == 0){
+            row_it->fill(0);
+            std::rotate(playfield.begin(), playfield.begin() + 1, row_it + 1 );
+            ++full_rows;
         }
     }
     
-    // int8_t incr;
-    // int8_t rowIncr;
-    // int8_t n;
-
-    // switch(rotation){
-    //     case 0:  rowIncr =  0 ;    incr =  1;    n = 0 ;   break;  // 0 degrees
-    //     case 1:  rowIncr =  17;    incr = -4;    n = 12;   break;  // 90 degrees
-    //     case 2:  rowIncr =  0 ;    incr = -1;    n = 15;   break;  // 180 degrees
-    //     default: rowIncr = -17;    incr =  4;    n = 3 ;   break;  // 270 degrees
-    // }
-    
-    // for(int8_t i =tetromino_y; i<tetromino_y + 4 ; ++i, n+=rowIncr){
-    //     for(int8_t j =tetromino_x; j< tetromino_x +4; ++j, n+=incr){
-    //         if(TetrominoShapes[currentTetromino][n] != 0){
-    //             given_playfield[i][j]=TetrominoShapes[currentTetromino][n];
-    //         }
-    //     }
-    // }
+    increase_score(full_rows);
 }
 
 
+void Tetris::spawn_tetromino_if_fits(){
+    constexpr const Point origin{(width / 2) - 2, height - 5};
+    const auto& tetromino_template = tetromino_shapes[random(7)];
 
-const bool Tetris::is_space_free(const int8_t& y, const int8_t& x) const{
-    // Check if space is out of bounds and if it isn't occupied.
-    return x >= 0 && x < WIDTH && y >= 0 && playfield[y][x] == 0;
-}
+    int8_t found = 0;
+    array<Point, 4> positions;
 
+    for(Point p{0,0}; p.y < tetromino_template.size();         ++p.y){
+        for(p.x = 0 ; p.x < tetromino_template.front().size(); ++p.x){
+            if(tetromino_template[p.y+1][p.x] != 0){
+                positions[found] = origin + p;
+                ++found;
+    }   }   }
 
-const bool Tetris::does_tetromino_fit() const{
-    auto is_space_free = [&](const int8_t& y, const int8_t& x){ return x >= 0 && x < WIDTH && y >= 0 && playfield[y][x] == 0; };
-
-
-    for(int8_t i = tetromino_x; i < tetromino_x + 4; ++i){
-        for(int8_t j = tetromino_y; j < tetromino_y + 4; ++j){
-            if(TetrominoShapes[currentTetromino][i + ((tetromino_y-j) * 4)] != 0){
-                if( !is_space_free(i, j) ){
-                    return false;
-                }
-                
-            }
-        }
+    gameover = !does_tetromino_fit(positions);
+    if(!gameover){
+        tetromino_origin = origin;
+        tetromino_positions = positions;
+        set_tetromino(1);
     }
-    return true;
-  
-
-
-    }
-
-
-void Tetris::spawn_new_tetromino(){
-    rotation = 0;
-    tetromino_y = HEIGHT - 5;
-    tetromino_x = (WIDTH / 2) - 2;
-    currentTetromino = random(28);
-
-    gameOver = !does_tetromino_fit();
 }
 
 
-void Tetris::increase_score(const int8_t & fullRows){
+void Tetris::increase_score(const int8_t & full_rows){
     // https://tetris.wiki/Scoring
     // Original BPS scoring system
-    switch(fullRows){
+    switch(full_rows){
         case 1: score += 40u;   break;
         case 2: score += 100u;  break;
         case 3: score += 300u;  break;
@@ -187,7 +127,25 @@ void Tetris::increase_score(const int8_t & fullRows){
         default:                break;
     }
 }
-    
 
-// template<int8_t HEIGHT, int8_t WIDTH> constexpr byte Tetris<HEIGHT, WIDTH>::TetrominoShapes[7][16];
-// template<int8_t WIDTH, int8_t HEIGHT> constexpr byte Tetris<WIDTH, HEIGHT>::TetrominoShapes[8];
+
+Point Tetris::get_rotated_position(const Point& origin, const Point& position, const int8_t& rotation) const{
+    uint8_t pi;
+    Point p = position - origin;
+
+    switch(rotation % 4){
+        case 0: pi = p.y * 4 + p.x;        break;
+        case 1: pi = 12 + p.y - (p.x * 4); break;
+        case 2: pi = 15 - (p.y * 4) - p.x; break;
+        default:pi = 3 - p.y + (p.x * 4);  //case 3:
+    }
+
+    p.x = pi % 4;
+    p.y = pi / 4;
+
+    return p + origin;
+}
+
+
+
+
--- lib/tetris/tetris.h
+++ lib/tetris/tetris.h
@@ -5,109 +5,117 @@
 #include <random>
 #include <algorithm>
 
+using std::array;
+
+
+
+struct Point{
+    int8_t x, y;
+
+Point& operator+=(const Point& other){
+    this->x += other.x;
+    this->y += other.y;
+    return *this;
+}
+
+Point& operator-=(const Point& other){
+    this->x -= other.x;
+    this->y -= other.y;
+    return *this;
+}
+
+
+Point operator+(const Point& other) const{
+    Point answer;
+    answer.x = this->x + other.x;
+    answer.y = this->y + other.y;
+    return answer;
+}
+
+Point operator-(const Point& other) const{
+    Point answer;
+    answer.x = this->x - other.x;
+    answer.y = this->y - other.y;
+    return answer;
+}
+
+};
+
 
 class Tetris{
     constexpr static int8_t width = 10;
     constexpr static int8_t height = 20;
+
+    Point tetromino_origin;
+    array<Point, 4> tetromino_positions; 
+
 public:
-    void begin();
+    void    begin(),
+            move_left(),
+            move_right(),
+            move_down();
 
-    void move_left();
-    void move_right();
-    void move_down();
-    void rotate_right();
-    void rotate_left();
+private:
+    void    check_rows(),
+            spawn_tetromino_if_fits(),
+            set_tetromino(const int8_t& value),
+            increase_score(const int8_t& full_rows);
+            
 
+    array<Point, 4> get_new_tetromino_positions (const Point& delta, const int8_t& rotation) const;
+    bool  move_tetromino_if_fits (const Point& delta, const int8_t& rotation);
+    bool  does_tetromino_fit     (const array<Point, 4>& points) const;
+    Point get_rotated_position   (const Point& origin, const Point& old_position, const int8_t& rotation) const;
 
-    void check_rows();
-    void place_tetromino();
-    const bool does_tetromino_fit() const;
-    void spawn_tetromino();
-    void increase_score(const int8_t & fullRows);
- 
-    
-
-//========================================================================//
-//                              VARIABLES                                 //
-//========================================================================//
-//private:
-    // std::array<std::array<byte, width>, height> playfield;
-     std::array<std::array<byte, width>, height> playfield;
-    
+protected:
+    array<array<int8_t, width>, height> playfield;
     bool gameover;
-    ulong score;
-    int8_t currentTetromino;
-    uint8_t rotation;
-    Position tetromino_origin;
-   
-    
+    size_t score;
 
-    // // Z  S  L  J T O I
-    // // 64 32 16 8 4 2 1
+    // Z  S  L  J T O I
+    // 64 32 16 8 4 2 1
     // static constexpr byte TetrominoShapes[8]{
     //     0b00000000u, 0b01001110u, 0b01101110u, 0b00111100u,
     //     0b00000001u, 0b00110011u, 0b01110111u, 0b01011001u 
     // };
-    
-   
 
-    const array<array<array<int8_t, 4>,4>,7> TetrominoShapes {{
+    const array<array<array<bool, 4>,2>,7> tetromino_shapes {{
 
-         // I-tetromino
-        {{
+        {{ // I-tetromino
         {{0,0,0,0}},
-        {{0,0,0,0}},
-        {{1,1,1,1}},
-        {{0,0,0,0}}
+        {{1,1,1,1}}
         }},
 
-        // O-tetromino
-        {{
-        {{0,0,0,0}},
+        {{ // O-tetromino
         {{0,1,1,0}},
-        {{0,1,1,0}},
-        {{0,0,0,0}}
+        {{0,1,1,0}}
         }},
 
-        // T-tetromino
-        {{
-        {{0,0,0,0}},
+        {{ // T-tetromino
         {{0,1,1,1}},
-        {{0,0,1,0}},
-        {{0,0,0,0}}
+        {{0,0,1,0}}
         }},
 
-        // L-tetromino
-        {{
-        {{0,0,0,0}},
+        {{ // L-tetromino
         {{0,1,1,1}},
-        {{0,1,0,0}},
-        {{0,0,0,0}}
+        {{0,1,0,0}}
         }},
 
-        // J-tetromino
-        {{
-        {{0,0,0,0}},
+        {{ // J-tetromino
         {{0,1,1,1}},
-        {{0,0,0,1}},
-        {{0,0,0,0}}
+        {{0,0,0,1}}
         }},
 
-        // S-tetromino
-        {{
-        {{0,0,0,0}},
+        {{ // S-tetromino
         {{0,0,1,1}},
-        {{0,1,1,0}},
-        {{0,0,0,0}}
+        {{0,1,1,0}}
         }},
 
-        // Z-tetromino
-        {{
-        {{0,0,0,0}},
+        {{  // Z-tetromino
         {{0,1,1,0}},
-        {{0,0,1,1}},
-        {{0,0,0,0}}
+        {{0,0,1,1}}
         }}
     }};
+
 };
 
